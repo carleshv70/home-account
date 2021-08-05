7b365acc184314d4dece8f8eec4b44b52042d9ce
Carles Huix <carleshv70@gmail.com>
2021-08-05 07:50:32 +0200

    Preparar pagina principal de medios de pago

--- home-account/home-account-webapp/src/main/java/com/chuix/home/account/controllers/view/PaymentMethodViewController.java
+++ home-account/home-account-webapp/src/main/java/com/chuix/home/account/controllers/view/PaymentMethodViewController.java
@@ -1,36 +1,38 @@
-package com.chuix.home.account.controllers.api;
+package com.chuix.home.account.controllers.view;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.chuix.home.account.domain.services.PaymentMethodService;
 import com.chuix.home.account.dto.PaymentMethodDto;
 import com.chuix.home.account.dto.mapper.PaymentMethodMapper;
 
-@RestController
-@RequestMapping("/api/medios-pago")
-public class PaymentMethodController {
-	
+@Controller
+@RequestMapping("/medios-pago")
+public class PaymentMethodViewController {
+
 	@Autowired
 	private PaymentMethodMapper mapper;
-	
+
 	@Autowired
 	private PaymentMethodService serive;
 
 	@GetMapping("/")
-	public ResponseEntity<?> getPaymentMethods() {
-		
-		List<PaymentMethodDto> pms = this.serive.getPaymentMethods().stream()
-				.map(pm -> this.mapper.mapToDto(pm))
+	public String interpretText(Model model) {
+
+		List<PaymentMethodDto> pms = this.serive.getPaymentMethods().stream().map(pm -> this.mapper.mapToDto(pm))
 				.collect(Collectors.toList());
-		
-		return new ResponseEntity<List<PaymentMethodDto>>(pms, HttpStatus.OK);
+
+		model.addAttribute("pms", pms);
+
+		return "paymentmethod";
+
 	}
+
 }
