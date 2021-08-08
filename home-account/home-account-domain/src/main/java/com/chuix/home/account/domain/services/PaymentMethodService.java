8dbcffd169375e14baab55d2ea4e5b2ae7d02270
Carles Huix <carleshv70@gmail.com>
2021-08-08 22:21:20 +0200

    Corrgir branch

--- home-account/home-account-domain/src/main/java/com/chuix/home/account/domain/services/PaymentMethodService.java
+++ home-account/home-account-domain/src/main/java/com/chuix/home/account/domain/services/PaymentMethodService.java
@@ -7,15 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.chuix.home.account.domain.entity.PaymentMethodEntity;
+import com.chuix.home.account.domain.exception.BusinessException;
+import com.chuix.home.account.domain.exception.BusinessExceptionEnum;
 import com.chuix.home.account.domain.mapper.PaymentMethodMapper;
 import com.chuix.home.account.persistence.dao.PaymentMethodDao;
 
 @Service
 public class PaymentMethodService {
-	
+
 	@Autowired
 	private PaymentMethodMapper mapper;
-	
+
 	@Autowired
 	private PaymentMethodDao pmDao;
 
@@ -28,20 +30,39 @@ public class PaymentMethodService {
 		paymentMethods.add(pm);
 		return paymentMethods;
 	}
-	
-	public PaymentMethodEntity addPaymentMethod(PaymentMethodEntity pm) {
-		
-		try {
-			PaymentMethodEntity pmDB = this.mapper.mapToEntity( this.pmDao.findByAccountNumber(pm.getAccountNumber()));			
-		}
-		catch( jpaEx)
 
-		
-		if (pmDB != null) {
-			throw new 
+	public PaymentMethodEntity addPaymentMethod(PaymentMethodEntity pm) throws BusinessException {
+
+		// Check if exist
+		if (getPaymentMethodByAccountNumber(pm.getAccountNumber()) != null) {
+			throw new BusinessException(BusinessExceptionEnum.BE0001);
 		}
-		
-		return null;
-		
+		return this.save(pm);
 	}
+
+	private PaymentMethodEntity getPaymentMethodByAccountNumber(String accountNumber) throws BusinessException {
+		PaymentMethodEntity pm;
+		try {
+			pm = this.mapper.mapToEntity(this.pmDao.findByAccountNumber(accountNumber));
+		} catch (Exception ex) {
+			throw new BusinessException(BusinessExceptionEnum.AE0002, ex, ex.getMessage());
+		}
+		return pm;
+	}
+	
+	private PaymentMethodEntity save(PaymentMethodEntity pm) throws BusinessException {
+		PaymentMethodEntity pmSaved;
+		try {
+			pmSaved = this.mapper.mapToEntity(this.pmDao.save(this.mapper.mapToJPA(pm)));	
+		}
+		catch( Exception ex) {
+			throw new BusinessException(BusinessExceptionEnum.AE0001, ex, ex.getMessage());
+		}
+		return pmSaved;
+
+	}
+	
+	
+	
+
 }
